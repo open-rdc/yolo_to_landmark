a898912c077b5a3a23b14500ce2f766ce0239e5f
Yasuo Hayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2022-09-04 12:54:17 +0900

    executable

--- scripts/yolo_to_landmark.py
+++ scripts/yolo_to_landmark.py
@@ -5,7 +5,7 @@ import rospy
 import rosparam
 from yolov5_pytorch_ros.msg import BoundingBoxes
 import math
-from emcl.msg import DetectObjects
+from emcl.msg import DetectObjects, DetectObject
 
 class yolo_to_landmark:
     def __init__(self):
@@ -14,13 +14,14 @@ class yolo_to_landmark:
         self.pub = rospy.Publisher("/detectobjects", DetectObjects, queue_size=1)
 
     def cb_yolo(self, data):
-        detect_objects = []
+        detect_objects = DetectObjects()
         for bounding_box in data.bounding_boxes:
-            detect_object = DetectObjects()
+            detect_object = DetectObject()
             detect_object.name = bounding_box.Class
-            detect_object.angle = -((bounding_box.ymax + bounding_box.ymin)/2 - self.width/2) / (self.width/2) * math.pi
+            detect_object.yaw = -(float(bounding_box.xmax + bounding_box.xmin)/2 - self.width/2) / (self.width/2) * math.pi
             detect_object.distance = 0
-            detect_objects.append(detect_object)
+            #print(str(detect_object.name)+", yaw: "+str(detect_object.yaw))
+            detect_objects.detectobjects.append(detect_object)
         self.pub.publish(detect_objects)
 
 if __name__ == '__main__':
